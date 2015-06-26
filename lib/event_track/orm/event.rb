9fceab7b4faff3a36849d3f0f5fc11bbc0dbc03c
Jerry Tao <taojay315@gmail.com>
2015-06-26 18:09:14 +0800

    sirialize parameters

--- lib/event_track/orm/event.rb
+++ lib/event_track/orm/event.rb
@@ -2,6 +2,7 @@ module EventTrack
   class Event < ActiveRecord::Base
     # TODO 支持多种ORM
     self.table_name = EventTrack.config.table_name
+    serialize :parameters
     belongs_to :trackable, polymorphic: true
     belongs_to :owner, polymorphic: true
   end
